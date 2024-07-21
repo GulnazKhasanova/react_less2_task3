74b02898bac881c52bb9d4c75d90dc76536bfd9d
Gulnaz <viteewato@mail.ru>
2024-07-21 21:52:22 +0300

    mew_project without jsx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createElement } from 'react';
 import './index.css';
-import { App } from './App';
+import { App }  from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+	createElement(App),
+	 document.getElementById('root')
+)
+
+
+
 
